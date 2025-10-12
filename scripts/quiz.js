26dd55fb5441c1c605dfc2bfcf0eb069af3d2972
Kelly Finnegan <kelly@Kellys-MacBook-Pro.local>
2025-10-12 12:10:42 -0400

    working

---
+++ scripts/quiz.js
@@ -0,0 +1,7 @@
+var questionNum = 0;
+var questions = {
+    "Select the state you think you are heading too?": "WY",
+    "How many layovers to get to your destination?": 1,
+    "Select the state where the layover is with the airport code of DFW": "TX",
+    "Select the region of the US where your destination is located": "West"
+}
